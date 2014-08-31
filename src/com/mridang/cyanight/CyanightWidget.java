b13c36aac286d479d1f3c3dbbef4f36f110725f8
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-08-31 11:32:00 +0300

    Made minor fixes, lowered SDK requirements and used ACRA instead of Bugsense

--- src/com/mridang/cyanight/CyanightWidget.java
+++ src/com/mridang/cyanight/CyanightWidget.java
@@ -4,18 +4,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
+import org.acra.ACRA;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.util.Log;
 
-import com.bugsense.trace.BugSenseHandler;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 
@@ -31,7 +32,7 @@ public class CyanightWidget extends DashClockExtension {
 
 		super.onCreate();
 		Log.d("CyanightWidget", "Created");
-		BugSenseHandler.initAndStartSession(this, getString(R.string.bugsense));
+		ACRA.init(new AcraApplication(getApplicationContext()));
 
 	}
 
@@ -41,7 +42,7 @@ public class CyanightWidget extends DashClockExtension {
 	 * (int)
 	 */
 	@Override
-	protected void onUpdateData(int arg0) {
+	protected void onUpdateData(int intReason) {
 
 		Log.d("CyanightWidget", "Checking nightly commits");
 		ExtensionData edtInformation = new ExtensionData();
@@ -62,7 +63,8 @@ public class CyanightWidget extends DashClockExtension {
 					edtInformation.expandedTitle(getResources().getQuantityString(R.plurals.changes, intDays, intDays));
 					edtInformation.expandedBody(HelperFunctions.getBuildString());
 
-					ComponentName comp = new ComponentName("com.cyanogenmod.updater", "com.cyanogenmod.updater.UpdatesSettings");
+					ComponentName comp = new ComponentName("com.cyanogenmod.updater",
+							"com.cyanogenmod.updater.UpdatesSettings");
 					Intent ittUpdater = new Intent().setComponent(comp);
 					edtInformation.clickIntent(ittUpdater);
 
@@ -74,7 +76,7 @@ public class CyanightWidget extends DashClockExtension {
 				Log.d("CyanightWidget", "Not a nightly build");
 			}
 
-			if (new Random().nextInt(5) == 0) {
+			if (new Random().nextInt(5) == 0 && !(0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))) {
 
 				PackageManager mgrPackages = getApplicationContext().getPackageManager();
 
@@ -85,22 +87,26 @@ public class CyanightWidget extends DashClockExtension {
 				} catch (NameNotFoundException e) {
 
 					Integer intExtensions = 0;
-				    Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
-				    String strPackage;
+					Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
+					String strPackage;
 
-				    for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
+					for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
 
-				    	strPackage = info.serviceInfo.applicationInfo.packageName;
-						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0); 
+						strPackage = info.serviceInfo.applicationInfo.packageName;
+						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0);
 
 					}
 
 					if (intExtensions > 1) {
 
 						edtInformation.visible(true);
-						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=com.mridang.donate")));
+						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri
+								.parse("market://details?id=com.mridang.donate")));
 						edtInformation.expandedTitle("Please consider a one time purchase to unlock.");
-						edtInformation.expandedBody("Thank you for using " + intExtensions + " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
+						edtInformation
+								.expandedBody("Thank you for using "
+										+ intExtensions
+										+ " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
 						setUpdateWhenScreenOn(true);
 
 					}
@@ -114,7 +120,7 @@ public class CyanightWidget extends DashClockExtension {
 		} catch (Exception e) {
 			edtInformation.visible(false);
 			Log.e("CyanightWidget", "Encountered an error", e);
-			BugSenseHandler.sendException(e);
+			ACRA.getErrorReporter().handleSilentException(e);
 		}
 
 		edtInformation.icon(R.drawable.ic_dashclock);
@@ -130,7 +136,6 @@ public class CyanightWidget extends DashClockExtension {
 
 		super.onDestroy();
 		Log.d("CyanightWidget", "Destroyed");
-		BugSenseHandler.closeSession(this);
 
 	}
 
--- src/com/mridang/cyanight/HelperFunctions.java
+++ src/com/mridang/cyanight/HelperFunctions.java
@@ -6,109 +6,108 @@ import java.io.InputStreamReader;
 
 import android.util.Log;
 
-
 /*
  * This class contains very simple helper-functions
  */
 public class HelperFunctions {
-	
+
 	/*
 	 * This method returns the version of the build
 	 * 
-	 * @returns   The version of the build
+	 * @returns The version of the build
 	 */
 	public static String getVersion() {
 
 		String strBuild = getBuildString();
-	    if (strBuild != null && strBuild.split("-").length == 4) {
-	    	return strBuild.split("-")[0];
-	    }
-	    return "";
-		
+		if (strBuild != null && strBuild.split("-").length == 4) {
+			return strBuild.split("-")[0];
+		}
+		return "";
+
 	}
 
 	/*
 	 * This method returns the type of the build
 	 * 
-	 * @returns   The type of the build
+	 * @returns The type of the build
 	 */
 	public static String getType() {
 
 		String strType = getBuildString();
-	    if (strType != null && strType.split("-").length == 4) {
-	    	return strType.split("-")[2];
-	    }
-	    return "";
-		
+		if (strType != null && strType.split("-").length == 4) {
+			return strType.split("-")[2];
+		}
+		return "";
+
 	}
-	
-    /*
-     * This method returns a system property using the getprop command
-     *
-     * @return   The value of the property if found, else null
-     */
-    public static String getBuildString() {
 
-    	String strValue = null;
-        BufferedReader birReader = null;
+	/*
+	 * This method returns a system property using the getprop command
+	 * 
+	 * @return The value of the property if found, else null
+	 */
+	public static String getBuildString() {
 
-        try {
+		String strValue = null;
+		BufferedReader birReader = null;
 
-        	Process p = Runtime.getRuntime().exec("getprop ro.modversion");
-            birReader = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
-            strValue = birReader.readLine();
-            birReader.close();
+		try {
 
-        } catch (IOException e) {
-            Log.e("HelperFunctions", "Unable to read property", e);
-        } finally {
+			Process p = Runtime.getRuntime().exec("getprop ro.modversion");
+			birReader = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
+			strValue = birReader.readLine();
+			birReader.close();
 
-        	if (birReader != null) {
+		} catch (IOException e) {
+			Log.e("HelperFunctions", "Unable to read property", e);
+		} finally {
 
-        		try {
+			if (birReader != null) {
 
-                	birReader.close();
+				try {
 
-                } catch (IOException e) {
-                    Log.e("HelperFunctions", "Exception while closing the file", e);
-                }
+					birReader.close();
 
-        	}
+				} catch (IOException e) {
+					Log.e("HelperFunctions", "Exception while closing the file", e);
+				}
 
-        }
-        
-        return strValue;
+			}
 
-    }
+		}
+
+		return strValue;
+
+	}
 
 	/*
 	 * This method returns the date of the build
 	 * 
-	 * @returns   The date of the build
+	 * @returns The date of the build
 	 */
 	public static String getDate() {
 
 		String strDate = getBuildString();
-	    if (strDate != null && strDate.split("-").length == 4) {
-	    	return strDate.split("-")[1];
-	    }
-	    return "";
-		
+		if (strDate != null && strDate.split("-").length == 4) {
+			return strDate.split("-")[1];
+		}
+		return "";
+
 	}
 
 	/*
 	 * This method returns the model of the build
 	 * 
-	 * @returns   The version of the build
+	 * @returns The version of the build
 	 */
 	public static String getModel() {
 
 		String strModel = getBuildString();
-	    if (strModel != null && strModel.split("-").length == 4) {
-	    	return strModel.split("-")[3];
-	    }
-	    return "";
-		
+		if (strModel != null && strModel.split("-").length == 4) {
+			return strModel.split("-")[3];
+		}
+		return "";
+
 	}
-    
+
 }
